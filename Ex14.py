4063162f4ab909da98f04d6933e8b78a5b6d272e
Perry Brandiezs <pbrandiezs@gmail.com>
2018-07-14 18:51:20 -0700

    First commit

---
+++ Ex14.py
@@ -0,0 +1,13 @@
+
+def uniques(x):
+    return(list(set(x)))
+
+
+a = [1,2,2,3,4]
+
+print(a)
+
+print(uniques(a))
+
+names = ["Michele", "Robin", "Sara", "Michele"]
+print(uniques(names))
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # python-ex14
+https://www.practicepython.org/exercise/2014/05/15/14-list-remove-duplicates.html
